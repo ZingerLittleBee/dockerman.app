510613de6fb1e67a54c5875c37ddc2708d6a382c
ZingerLittleBee <6970999@gmail.com>
2025-05-05 02:19:26 +0800

    ✨ chore: update latestVersion to 2.1.1 in siteConfig

--- src/app/siteConfig.ts
+++ src/app/siteConfig.ts
@@ -14,7 +14,7 @@ export const siteConfig = {
     privacy: "/privacy",
     terms: "/terms",
   },
-  latestVersion: "2.1.0",
+  latestVersion: "2.1.1",
 }
 
 export type siteConfig = typeof siteConfig
