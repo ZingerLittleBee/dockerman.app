f6ad905453b5e8c8cf7c7e5a4b9f99d327e53dc9
ZingerLittleBee <6970999@gmail.com>
2026-02-23 18:27:44 +0800

    fix: enable Web Vitals capture in PostHog config

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,5 +5,6 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: '2025-11-30',
   capture_exceptions: true,
+  capture_performance: { web_vitals: true },
   debug: process.env.NODE_ENV === 'development'
 })
