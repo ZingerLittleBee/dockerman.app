ee8afa3205730683c488ac465b120371b23748ae
ZingerLittleBee <6970999@gmail.com>
2026-02-28 21:09:59 +0800

    feat: update mdx-components with fumadocs defaults

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,33 +1,18 @@
 import type { MDXComponents } from 'mdx/types'
-import {
-  Bold,
-  ChangelogEntry,
-  ChangelogImage,
-  CustomLink,
-  H1,
-  H2,
-  H3,
-  P,
-  Ul
-} from '@/components/mdx'
+import defaultMdxComponents from 'fumadocs-ui/mdx'
 
-const customComponents = {
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  p: P,
-  Bold,
-  ul: Ul,
-  a: CustomLink,
-  ChangelogEntry,
-  ChangelogImage
-}
-
+// Fumadocs uses getMDXComponents
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...customComponents,
+    ...defaultMdxComponents,
     ...components
   }
 }
 
-export const useMDXComponents = getMDXComponents
+// Next.js MDX integration uses useMDXComponents (for changelog pages)
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    ...defaultMdxComponents,
+    ...components
+  }
+}
